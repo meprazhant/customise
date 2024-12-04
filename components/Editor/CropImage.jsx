f3b2f6ae5c31ce54e5c35939fb8d1cb28924e90f
nishant0919 <nishantkafle20@gmail.com>
2024-12-04 21:15:28 +0545

    Add CropImage component and integrate Cropper.js for image cropping functionality

---
+++ components/Editor/CropImage.jsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import Cropper from "cropperjs";
+import "cropperjs/dist/cropper.css"; // Ensure Cropper CSS is imported
+
+function CropImage({ image, setCropedPic, aspect, setShowCrop }) {
+  const imgref = useRef(null);
+
+  useEffect(() => {
+    const cropper = new Cropper(document.getElementById("img"), {
+      autoCrop: false,
+      aspectRatio: aspect[0] / aspect[1],
+      crop(event) {
+        saveCrop(event);
+      },
+    });
+    return () => {
+      cropper.destroy();
+    };
+  }, []);
+
+  function saveCrop(event) {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = aspect[0] * 1000;
+    canvas.height = aspect[1] * 1000;
+    ctx.drawImage(
+      imgref.current,
+      event.detail.x,
+      event.detail.y,
+      event.detail.width,
+      event.detail.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+    canvas.toBlob((blob) => {
+      setCropedPic(blob);
+    });
+  }
+
+  return (
+    <div className="flex flex-col gap-5 p-2 ">
+      <img
+        ref={imgref}
+        id="img"
+        src={
+          image ? URL.createObjectURL(image) : "https://via.placeholder.com/200"
+        }
+        alt="crop"
+        style={{ height: "450px", width: "450px" }}
+      />
+    </div>
+  );
+}
+
+export default CropImage;
--- components/Editor/EditorMain.jsx
+++ components/Editor/EditorMain.jsx
@@ -1,27 +1,28 @@
 "use client";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
+import CropImage from "./CropImage";
 
 const Designs = [
   {
     image: "https://i.ibb.co/PDMTbdG/image.png",
     title: "airpods",
-    aspect: 1,
+    aspect: [1, 1],
   },
   {
     image: "https://i.ibb.co/jhxVBv8/MOUSEPAD-TEMPLETE-UPDATED.png",
     title: "mousepads",
-    aspect: 1,
+    aspect: [1, 0.43],
   },
   {
     image: "https://i.ibb.co/qY1SVXS/laptop.png",
     title: "laptopsleeves",
-    aspect: 1,
+    aspect: [1, 0.6],
   },
   {
     image: "https://i.ibb.co/dMxR0Kg/POP-SOCKET-TEPLETE-UPDATED.png",
     title: "popsockets",
-    aspect: 1,
+    aspect: [1, 1],
   },
 ];
 
@@ -29,6 +30,7 @@ function EditorMain({ id }) {
   const [image, setImage] = useState(null);
   const [images, setImages] = useState([]);
   const [showCrop, setShowCrop] = useState(false);
+  const [cropedPic, setCropedPic] = useState(null);
   useEffect(() => {
     // scroll to editor
     const editor = document.getElementById("editor");
@@ -37,17 +39,6 @@ function EditorMain({ id }) {
 
   const design = Designs.find((design) => design.title === id);
   const imgref = useRef(null);
-
-  function cropImage() {
-    const el = document.getElementById("image");
-    
-  }
-
-  useEffect(() => {
-    if (image) {
-      cropImage();
-    }
-  }, [image]);
 
   return (
     <div className="flex flex-col gap-5 p-2">
@@ -71,7 +62,7 @@ function EditorMain({ id }) {
         {/* preview Screen */}
         <div
           className="
-                w-full bg-red-400 shadow-md rounded-md border-dashed border-2 border-gray-300 h-full
+                w-full bg-red-400 relative shadow-md rounded-md border-dashed border-2 border-gray-300 h-full
                 "
         >
           <img
@@ -79,6 +70,14 @@ function EditorMain({ id }) {
             src={`${design.image}`}
             alt="preview"
             className="w-full  object-contain"
+            style={{
+              backgroundImage: `url(${
+                cropedPic ? URL.createObjectURL(cropedPic) : ""
+              })`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
           />
         </div>
         {/* editor */}
@@ -87,124 +86,137 @@ function EditorMain({ id }) {
                 w-full bg-white shadow-md rounded-md p-2 border-dashed border-2 border-gray-300 h-full flex flex-col gap-3
                 "
         >
-          <h1 className="text-2xl font-bold text-center">
-            Customise <span className="capitalize ">{design.title}</span>
-          </h1>
-          <p className="text-gray-400 text-center">
-            Customise your {design.title} with your own design
-          </p>
-          {/* write a short description */}
-          <p className="text-gray-700 ">
-            How to customise your {design.title}? <br />
-            1. Click on the upload button below <br />
-            2. Upload your image <br />
-            3. Resize and adjust the image <br />
-            4. Click on the Order button <br />
-            5. Fill in the details and place the order
-          </p>
-          {/* images */}
-          <div className="flex gap-2 flex-col">
-            {!!image && (
-              <div className="flex">
-                <p className="text-gray-900">Choosen Design</p>
-                <img
-                  src={URL.createObjectURL(image)}
-                  alt="custom"
-                  className="w-20 h-20 object-contain border border-transparent"
-                />
-              </div>
-            )}
-            {images.length !== 0 && (
-              <div className="flex flex-col gap-3">
-                <p className="text-gray-900">Your Photos</p>
-                <div className="flex flex-wrap gap-3">
-                  {images.map((img, index) => (
+          {(!showCrop && (
+            <div className="flex flex-col gap-5">
+              <h1 className="text-2xl font-bold text-center">
+                Customise <span className="capitalize ">{design.title}</span>
+              </h1>
+              <p className="text-gray-400 text-center">
+                Customise your {design.title} with your own design
+              </p>
+              {/* write a short description */}
+              <p className="text-gray-700 ">
+                How to customise your {design.title}? <br />
+                1. Click on the upload button below <br />
+                2. Upload your image <br />
+                3. Resize and adjust the image <br />
+                4. Click on the Order button <br />
+                5. Fill in the details and place the order
+              </p>
+              {/* images */}
+              <div className="flex gap-2 flex-col">
+                {!!image && (
+                  <div className="flex flex-col">
+                    <p className="text-gray-900">Choosen Design</p>
                     <img
-                      onClick={() => {
-                        setImage(img);
-                        setShowCrop(true);
-                      }}
-                      key={index}
-                      src={URL.createObjectURL(img)}
+                      src={URL.createObjectURL(image)}
                       alt="custom"
                       className="w-20 h-20 object-contain border border-transparent"
-                      style={{
-                        border:
-                          image === img
-                            ? "2px solid blue"
-                            : "2px solid transparent",
-                      }}
                     />
-                  ))}
-                </div>
+                  </div>
+                )}
+                {images.length !== 0 && (
+                  <div className="flex flex-col gap-3">
+                    <p className="text-gray-900">Your Photos</p>
+                    <div className="flex flex-wrap gap-3">
+                      {images.map((img, index) => (
+                        <img
+                          onClick={() => {
+                            setImage(img);
+                            setShowCrop(true);
+                          }}
+                          key={index}
+                          src={URL.createObjectURL(img)}
+                          alt="custom"
+                          className="w-20 h-20 object-contain border border-transparent"
+                          style={{
+                            border:
+                              image === img
+                                ? "2px solid blue"
+                                : "2px solid transparent",
+                          }}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-
-          <div className="flex gap-2 justify-center items-center p-5 border-dashed rounded-md ">
-            <input
-              onChange={(e) => {
-                if (e.target.files.length === 0) return;
-                setImage(e.target.files[0]);
-                setImages([...images, e.target.files[0]]);
-                setShowCrop(true);
-              }}
-              type="file"
-              id="file"
-              accept="image/*"
-              className="hidden"
-            />
-            <label
-              htmlFor="file"
+              <div className="flex gap-2 justify-center items-center p-5 border-dashed rounded-md ">
+                <input
+                  onChange={(e) => {
+                    if (e.target.files.length === 0) return;
+                    setImage(e.target.files[0]);
+                    setImages([...images, e.target.files[0]]);
+                    setShowCrop(true);
+                  }}
+                  type="file"
+                  id="file"
+                  accept="image/*"
+                  className="hidden"
+                />
+                <label
+                  htmlFor="file"
+                  className="
+                          bg-blue-400 text-white px-5 py-2 rounded-md cursor-pointer
+                          "
+                >
+                  Upload Image
+                </label>
+                <button
+                  className="
+                          bg-red-400 text-white px-5 py-2 rounded-md cursor-pointer
+                          "
+                >
+                  Order
+                </button>
+              </div>
+            </div>
+          )) || (
+            <div
               className="
-                        bg-blue-400 text-white px-5 py-2 rounded-md cursor-pointer
-                        "
+                   bg-white shadow-md rounded-md p-5 flex flex-col gap-5
+                   "
             >
-              Upload Image
-            </label>
-            <button
-              className="
-                        bg-red-400 text-white px-5 py-2 rounded-md cursor-pointer
-                        "
-            >
-              Order
-            </button>
-          </div>
+              <h1 className="text-2xl font-bold text-center">Crop Image</h1>
+              <div
+                className="
+                       w-full h-[500px] bg-gray-100 flex items-center justify-center
+                       "
+              >
+                <CropImage
+                  image={image}
+                  aspect={design.aspect}
+                  setCropedPic={setCropedPic}
+                  setShowCrop={setShowCrop}
+                />
+              </div>
+              <div className="flex gap-2 justify-center">
+                <button
+                  onClick={() => {
+                    setImage(cropedPic);
+                    setShowCrop(false);
+                  }}
+                  className="
+                           bg-blue-400 text-white px-5 py-2 rounded-md cursor-pointer
+                           "
+                >
+                  Save
+                </button>
+                <button
+                  onClick={() => {
+                    setShowCrop(false);
+                  }}
+                  className="
+                           bg-red-400 text-white px-5 py-2 rounded-md cursor-pointer
+                           "
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-
-      {/* crop image */}
-      {showCrop && (
-        <div
-          className="fixed backdrop-blur-sm
-        top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center"
-        >
-          <div
-            id="image-cropper"
-            className="
-                w-[400px]  bg-white p-5 rounded-md shadow-md flex flex-col gap-4
-                "
-          >
-            <img
-              id="cropit-preview"
-              src={URL.createObjectURL(image)}
-              alt="crop"
-              className="w-full h-full max-h-[70vh] object-contain"
-            />
-            <input type="range" className="cropit-image-zoom-input" />
-            <button
-              onClick={() => {
-                setShowCrop(false);
-              }}
-              className="
-                    bg-red-400 text-white px-5 py-2 rounded-md cursor-pointer
-                    "
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
